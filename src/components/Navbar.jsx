0028c21a8bf6ecdeff366570d39519c30a7612a1
Rasel Ahmed <raselahmediu22@gmail.com>
2026-05-12 08:33:19 +0600

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className='flex justify-between p-3 mx-auto container'>
+            <ul className='flex gap-3'>
+                <li><Link href={'/'}>Home</Link></li>
+                <li><Link href={'/destinations'}>Destinations</Link></li>
+                <li><Link href={'/my-bookings'}>My Bookings</Link></li>
+            </ul>
+
+            <div>
+                <Image src={'/assets/Wanderlast.png'} width={150} height={150} alt='wanderlast'></Image>
+            </div>
+
+            <ul className='flex gap-3'>
+                <li><Link href={'/profile'}>Profile</Link></li>
+                <li><Link href={'/login'}>Login</Link></li>
+                <li><Link href={'/signup'}>Sign Up</Link></li>
+            </ul>
+
+        </nav>
+    );
+};
+
+export default Navbar;
